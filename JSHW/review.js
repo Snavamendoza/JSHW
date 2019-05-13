5222568165049f6a0fd29fb5d8f205f97c6f80e2
Snavamendoza <50380016+Snavamendoza@users.noreply.github.com>
2019-05-13 10:15:57 -0500

    Add files via upload

---
+++ JSHW/review.js
@@ -0,0 +1,46 @@
+var Words = 'Como Estas?';
+var Test = false;
+var Numbers = 21;
+var Sentence = 'I am 21 years old';
+console.log(Words);
+console.log(Test);
+console.log(Numbers);
+console.log(Sentence);
+var equation1 = 20 + 1;
+var equation2 = 2019 - 1992;
+var equation3 = 5*5;
+console.log(equation1);
+console.log(equation2);
+console.log(equation3);
+var Groceries = [
+'Milk',
+'Honey',
+'Bread',
+'Tea',
+'Cookies'
+];
+console.log(Groceries);
+var numberList = [
+    1,2,3,4,5,6
+];
+console.log(numberList);
+console.log(numberList.splice(0,3));
+console.log(numberList);
+
+console.log(numberList);
+numberList.push(7);
+numberList.push(8);
+numberList.push(9);
+console.log(numberList);
+
+console.log(numberList);
+numberList.reverse();
+console.log(numberList);
+var name = 'Sandra';
+var fullSentence = `Hello ${name} I'm happy to announce you are free of your student loans!`;
+console.log(fullSentence);
+
+
+console.log(typeof Groceries);
+console.log(typeof numberList);
+console.log(typeof Test);
